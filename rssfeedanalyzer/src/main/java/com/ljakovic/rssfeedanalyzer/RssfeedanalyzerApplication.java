917d6b8b34ce79207baa7f27c7e4b11400b2fd72
Luka Jaković <ljakovi98@gmail.com>
2024-08-17 12:42:17 +0200

    project init

---
+++ rssfeedanalyzer/src/main/java/com/ljakovic/rssfeedanalyzer/RssfeedanalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.ljakovic.rssfeedanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssfeedanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssfeedanalyzerApplication.class, args);
+	}
+
+}
---
+++ rssfeedanalyzer/src/test/java/com/ljakovic/rssfeedanalyzer/RssfeedanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljakovic.rssfeedanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssfeedanalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
